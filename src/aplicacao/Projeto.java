5a5a78313d3d845a0c6f9e0b312da2cce175bf78
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-02 15:28:44 -0300

    Atualizando data de cadastro (NOVA DATA)

--- src/aplicacao/Projeto.java
+++ src/aplicacao/Projeto.java
@@ -23,10 +23,21 @@ public class Projeto {
 
 		if (!dataSaida.after(dataEntrada)) {
 			System.out.println("Erro na reserva, data de saída deve ser depois da data de entrada!  ");
-		}
+		} 
 		else {
 			Reserva reserva = new Reserva(numero, dataEntrada, dataSaida);
 			System.out.println("Reserva: " + reserva);
+
+			System.out.println();
+			System.out.println("Entre com a nova data da reserva: ");
+			System.out.print("Data de entrada: ");
+			dataEntrada = sdf.parse(sc.next());
+			System.out.print("Data saída: ");
+			dataSaida = sdf.parse(sc.next());
+
+			reserva.atualizarData(dataEntrada, dataSaida);
+			System.out.println("Reserva: " + reserva);
+
 		}
 		sc.close();
 
